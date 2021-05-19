1e5bfff78e6e1a184fd9ca4bc9b018652a96098a
Eric Nielsen <eric@amalgamar.com.br>
2021-05-18 23:36:52 -0500

    Add zinit-light

---
+++ frameworks/zinit-light.zsh
@@ -0,0 +1,39 @@
+() {
+local -r home_dir=${1}
+
+# download the repository
+command mkdir ${home_dir}/.zinit
+command git clone --quiet https://github.com/zdharma/zinit.git ${home_dir}/.zinit/bin
+
+# add modules to .zshrc
+print 'source ${HOME}/.zinit/bin/zinit.zsh
+zinit light "zimfw/environment"
+zinit ice autoload"git-alias-lookup;git-branch-current;git-branch-delete-interactive;git-dir;git-ignore-add;git-root;git-stash-clear-interactive;git-stash-recover;git-submodule-move;git-submodule-remove"
+zinit light "zimfw/git"
+zinit light "zimfw/input"
+zinit light "zimfw/termtitle"
+zinit ice autoload"mkcd;mkpw"
+zinit light "zimfw/utility"
+zinit ice autoload"duration-info-precmd;duration-info-preexec"
+zinit light "zimfw/duration-info"
+zinit ice autoload"coalesce;git-action;git-info"
+zinit light "zimfw/git-info"
+zinit light "zimfw/asciiship"
+zinit ice as"completion"
+zinit light "zsh-users/zsh-completions"
+zinit light "zsh-users/zsh-autosuggestions"
+zinit light "zsh-users/zsh-syntax-highlighting"
+zinit light "zsh-users/zsh-history-substring-search"
+
+autoload -Uz compinit
+compinit
+
+zinit cdreplay -q
+
+bindkey "^[[A" history-substring-search-up
+bindkey "^[[B" history-substring-search-down
+' >>! ${home_dir}/.zshrc
+
+# compile the plugins
+HOME=${home_dir} zsh -ic 'zinit compile --all; exit'
+} "${@}"
