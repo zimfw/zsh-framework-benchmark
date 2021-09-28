1851290df3834d1d606dfeccbddf5e9ca38a5b7f
Alexandros Kozák <agkozak@users.noreply.github.com>
2021-09-28 13:26:22 -0700

    Update frameworks/zcomet.zsh
    Co-authored-by: Eric Nielsen <ericbn+github@hey.com>

--- frameworks/zcomet.zsh
+++ frameworks/zcomet.zsh
@@ -19,23 +19,14 @@ zcomet load zsh-users/zsh-autosuggestions
 zcomet load zsh-users/zsh-syntax-highlighting
 zcomet load zsh-users/zsh-history-substring-search
 # zcomet adds functions to fpath but does not autoload them!
-autoload -Uz git-alias-lookup \
-         git-branch-current \
-         git-branch-delete-interactive \
-         git-dir \
-         git-ignore-add \
-         git-root \
-         git-stash-clear-interactive \
-         git-stash-recover \
-         git-submodule-move \
-         git-submodule-remove \
-         mkcd \
-         mkpw \
-         duration-info-precmd \
-         duration-info-prexec \
-         coalesce \
-         git-action \
-         git-info
+() {
+  setopt LOCAL_OPTIONS EXTENDED_GLOB
+  local zfunction
+  for zfunction in ${HOME}/.zcomet/repos/zimfw/*/functions/^(*~|*.zwc(|.old)|_*|prompt_*_setup)(N-.:t); do
+    autoload -Uz ${zfunction}
+  done
+}
+
 [[ $TERM != dumb ]] && zcomet compinit
 
 bindkey "^[[A" history-substring-search-up
