060c1666f64d7697211ceab1e14bd1ef66ad9fa3
Alexandros Kozak <git-agkozak@sneakemail.com>
2021-09-28 11:51:12 -0700

    Heredoc

--- frameworks/zcomet.zsh
+++ frameworks/zcomet.zsh
@@ -1,10 +1,11 @@
 () {
-local -r home_dir=${1}
+  local -r home_dir=${1}
 
-git clone https://github.com/agkozak/zcomet.git $home_dir
+  git clone https://github.com/agkozak/zcomet.git $home_dir
 
 # add modules to .zshrc
-print 'source ${HOME}/zcomet.zsh
+  >| ${home_dir}/.zshrc <<\END
+source ${HOME}/zcomet.zsh
 zcomet load zimfw/environment
 zcomet load zimfw/git
 zcomet load zimfw/input
@@ -18,27 +19,27 @@ zcomet load zsh-users/zsh-autosuggestions
 zcomet load zsh-users/zsh-syntax-highlighting
 zcomet load zsh-users/zsh-history-substring-search
 # zcomet adds functions to fpath but does not autoload them!
-autoload -Uz git-alias-lookup \\
-         git-branch-current \\
-         git-branch-delete-interactive \\
-         git-dir \\
-         git-ignore-add \\
-         git-root \\
-         git-stash-clear-interactive \\
-         git-stash-recover \\
-         git-submodule-move \\
-         git-submodule-remove \\
-         mkcd \\
-         mkpw \\
-         duration-info-precmd \\
-         duration-info-prexec \\
-         coalesce \\
-         git-action \\
+autoload -Uz git-alias-lookup \
+         git-branch-current \
+         git-branch-delete-interactive \
+         git-dir \
+         git-ignore-add \
+         git-root \
+         git-stash-clear-interactive \
+         git-stash-recover \
+         git-submodule-move \
+         git-submodule-remove \
+         mkcd \
+         mkpw \
+         duration-info-precmd \
+         duration-info-prexec \
+         coalesce \
+         git-action \
          git-info
 [[ $TERM != dumb ]] && zcomet compinit
 
 bindkey "^[[A" history-substring-search-up
 bindkey "^[[B" history-substring-search-down
-' >>! ${home_dir}/.zshrc
+END
 
 } "${@}"
