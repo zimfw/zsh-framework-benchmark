40f691ddbbee93024d1195fd6363fb934542d292
Eric Nielsen <eric@amalgamar.com.br>
2021-09-28 08:32:19 -0500

    Debug Zim startup time in login shell

--- frameworks/zim.zsh
+++ frameworks/zim.zsh
@@ -6,4 +6,15 @@ command curl -sS --proto -all,+https https://raw.githubusercontent.com/zimfw/ins
 
 # start login shell
 HOME=${home_dir} zsh -lc 'wait; exit'
+# command cp ${home_dir}/.zshrc{,.orig}
+# print '
+# unset CHARSET LANG LC_COLLATE
+# ' >! ${home_dir}/.zshrc
+# cat ${home_dir}/.zshrc.orig >>! ${home_dir}/.zshrc
+# print '
+# print sourced zshrc >>! ~/../calls.txt
+# ' >>! ${home_dir}/.zshrc
+# command rm -rf ${home_dir}/.zshrc.orig
+# HOME=${home_dir} zsh -lc 'exit'
+# ZDOTDIR=${home_dir} HOME=${home_dir} ZIM_HOME=${home_dir}/.zim zsh -c "source ${home_dir}/../../trace-zim && mv /tmp/ztrace.tar.gz ${home_dir}/.."
 } "${@}"
