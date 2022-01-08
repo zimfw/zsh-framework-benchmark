cffe46b50353a88c584334b1fc610961de73aa8e
Eric Nielsen <eric@amalgamar.com.br>
2022-01-07 20:55:58 -0500

    Add zim-branch

---
+++ frameworks/zim-branch3.zsh
@@ -0,0 +1,29 @@
+() {
+local -r home_dir=${1}
+
+# download the repository
+command curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | HOME=${home_dir} zsh
+
+command sleep 1
+# add modules to .zshrc
+>! ${home_dir}/.zimrc <<\END
+zmodule environment
+zmodule git
+zmodule input
+zmodule termtitle
+zmodule utility
+
+zmodule duration-info
+zmodule git-info
+zmodule asciiship
+
+zmodule zsh-users/zsh-completions --fpath src
+zmodule completion --branch compinit
+
+zmodule zsh-users/zsh-syntax-highlighting
+zmodule zsh-users/zsh-history-substring-search
+zmodule zsh-users/zsh-autosuggestions
+END
+
+HOME=${home_dir} zsh -ic 'zimfw update'
+} "${@}"
