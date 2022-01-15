8befc663cbebbfa32f5b02a028349e7a60bd92db
Eric Nielsen <eric@amalgamar.com.br>
2022-01-15 18:33:51 -0500

    No need to start a login Zsh to init Zim anymore

--- frameworks/zim.zsh
+++ frameworks/zim.zsh
@@ -2,8 +2,5 @@
 local -r home_dir=${1}
 
 # download the repository
-command curl -sS --proto -all,+https https://raw.githubusercontent.com/zimfw/install/master/install.zsh | HOME=${home_dir} zsh
-
-# start login shell
-HOME=${home_dir} zsh -lc 'exit'
+command curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | HOME=${home_dir} zsh
 } "${@}"
